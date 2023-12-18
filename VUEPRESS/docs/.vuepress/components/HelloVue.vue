262e6376e73a65802e669175d03a9de18b07bd9d
alna0077 <alna0077@algonquinlive.com>
2023-12-17 23:20:31 -0500

    final

---
+++ VUEPRESS/docs/.vuepress/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<template>
+     <div>
+         <h2>Hello from the 'HelloVue' component</h2>
+     </div>
+ </template>
+ 
+ <script>
+     export default{ }
+ </script>
+ 
+ <style>
+ </style>
+ 
